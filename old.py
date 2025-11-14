1b696ebf1b6c2a0f914dcb44247d1b8a194c3ca3
Dhanendra2 <dhanendraparihar2@gmail.com>
2025-11-14 22:45:41 +0530

    Add files via upload

---
+++ old.py
@@ -0,0 +1,43 @@
+#k
+## A, B, C= map(int, input().split())
+# minimum = min(A,B,C)
+# maximum = max(A,B,C )
+# print(minimum, maximum)
+#l
+## F1,S1= input().split()
+# F2,S2= input().split()
+# if S1==S2:
+#     print("ARE Brothers")
+# else:
+#     print("NOT") 
+#m
+# X=input()
+# if X.isdigit():
+#     print("IS DIGIT")
+# else:
+#     print("ALPHA")
+#     if X.isupper():
+#         print("IS CAPITAL")
+#     else:
+#         print("IS SMALL")    
+      #n
+      # X=input()
+#X=input()
+# if X.islower():
+#     print(X.upper())
+# else:
+#     print(X.lower())      
+#o
+# expr = input()
+# if '+' in expr:
+#     A, B = expr.split('+')
+#     print(int(A) + int(B))
+# elif '-' in expr:
+#     A, B = expr.split('-')
+#     print(int(A) - int(B))
+# elif '*' in expr:
+#     A, B = expr.split('*')
+#     print(int(A) * int(B))
+# elif '/' in expr:
+#     A, B = expr.split('/')
+#     print(int(A) / int(B)) 
